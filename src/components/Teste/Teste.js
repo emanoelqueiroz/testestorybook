371a9db21e04aeda73f0fbebf2f6ca3f7733b3d7
Emanoel Queiroz <emanoel.queiroz@viasoft.com.br>
2021-09-06 11:00:19 -0300

    ajuste componente

--- src/components/Teste/Teste.js
+++ src/components/Teste/Teste.js
@@ -5,9 +5,9 @@ export const Teste = () => {
   const [state, setstate] = useState(0)
 
   return (
-    <>
+    <div>
       <h1 onClick={() => setstate(state + 1)}>Teste {state}</h1>
       <Button variant="primary">Primary</Button>
-    </>
+    </div>
   );
 };
