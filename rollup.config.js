23748325f1cdcdf81fdbccdf539a9e1716990e8f
Emanoel Queiroz <emanoel.queiroz@viasoft.com.br>
2021-09-06 17:49:27 -0300

    tentative de ajuste do popper

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,17 @@ import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 
+// var arrow = require('@popperjs/core/lib/modifiers/arrow');
+// var computeStyles = require('@popperjs/core/lib/modifiers/computeStyles');
+// var eventListeners = require('@popperjs/core/lib/modifiers/eventListeners');
+// var flip = require('@popperjs/core/lib/modifiers/flip');
+// var hide = require('@popperjs/core/lib/modifiers/hide');
+// var offset = require('@popperjs/core/lib/modifiers/offset');
+// var popperOffsets = require('@popperjs/core/lib/modifiers/popperOffsets');
+// var preventOverflow = require('@popperjs/core/lib/modifiers/preventOverflow');
+// var enums = require('@popperjs/core/lib/enums');
+// var popperBase = require('@popperjs/core/lib/popper-base');
+
 // TODO: resolver external popper.js
 export default [
   {
@@ -19,6 +30,19 @@ export default [
         format: 'es',
         exports: 'named',
       }
+    ],
+    external: [
+      '@popperjs/core/lib/modifiers/arrow',
+      '@popperjs/core/lib/modifiers/computeStyles',
+      '@popperjs/core/lib/modifiers/eventListeners',
+      '@popperjs/core/lib/modifiers/flip',
+      '@popperjs/core/lib/modifiers/hide',
+      '@popperjs/core/lib/modifiers/offset',
+      '@popperjs/core/lib/modifiers/popperOffsets',
+      '@popperjs/core/lib/modifiers/preventOverflow',
+      '@popperjs/core/lib/enums',
+      '@popperjs/core/lib/popper-base',
+      'prop-types-extra/lib/isRequiredForA11y',
     ],
     plugins: [
       postcss({
@@ -35,6 +59,7 @@ export default [
       }),
       commonjs(),
       external(),
+      // comentar esse cara pra ver as coisa no index.js
       terser(),
     ]
   }
